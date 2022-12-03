09666e674aefa55dcc437bc2880f82f6ccf9ad2e
filmee24 <filmee24@gmail.com>
2022-12-03 19:47:43 +0100

    Add GUI Test

---
+++ Src/TestApp/TestGui/Module.cs
@@ -0,0 +1,28 @@
+﻿using AuroraModularis;
+
+namespace TestGui;
+
+[Priority(ModulePriority.Max)]
+public class Module : AuroraModularis.Module
+{
+    public override string Name => "Host";
+
+    public override Task OnStart()
+    {
+        var frm = new Form1();
+        frm.FormClosing += (s, e) =>
+        {
+            Environment.Exit(0);
+        };
+
+        Inbox.Subscribe<Button>(_ =>
+        {
+            frm.Controls.Add(_);
+        });
+
+        Application.DoEvents();
+        Application.Run(frm);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Src/TestApp/TestGui/Program.cs
@@ -0,0 +1,23 @@
+using AuroraModularis;
+
+namespace TestGui;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    private static Task Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+
+        return BootstrapperBuilder.StartConfigure()
+            .WithAppName("TestGui")
+            .WithModulesBasePath(Environment.CurrentDirectory)
+            .WithSettingsBasePath(".")
+            .BuildAndStartAsync();
+    }
+}
