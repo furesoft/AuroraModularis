9cd734cd66e1d2feb49e85a027ad6dd41465a070
Chris <filmee24@gmail.com>
2022-12-17 15:02:25 +0100

    Cleanup

--- Src/Modules/PathResolver/AuroraModularis.Modules.PathResolver.Models/IPathResolver.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AuroraModularis.Modules.PathResolver.Models;
-
-public interface IPathResolver
-{
-    string ConvertPath(string logFile);
-    void Map(string identifier, string path);
-
-    string Resolve(string identifier);
-}
