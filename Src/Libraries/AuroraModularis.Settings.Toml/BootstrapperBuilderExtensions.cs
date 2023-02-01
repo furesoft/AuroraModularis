2830842487e5f1520cca37eb833fa4ec9fc3ff68
furesoft <filmee24@gmail.com>
2023-02-01 08:45:02 +0100

    feat(3rd party): Add Toml SettingsProvider

---
+++ Src/Libraries/AuroraModularis.Settings.Toml/BootstrapperBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using AuroraModularis.Core;
+
+namespace AuroraModularis.Settings.Toml;
+
+public static class BootstrapperBuilderExtensions
+{
+    public static IBootstrapBuilder UseToml(this IBootstrapBuilder builder)
+    {
+        return builder.WithSettingsProvider<TomlSettingsProvider>();
+    }
+}
---
+++ Src/Libraries/AuroraModularis.Settings.Toml/TomlSettingsProvider.cs
@@ -0,0 +1,23 @@
+﻿using AuroraModularis.Core;
+using Tomlet;
+
+namespace AuroraModularis.Settings.Toml;
+
+public class TomlSettingsProvider : IModuleSettingsProvider
+{
+    public void Save(object data, string path)
+    {
+        var document = TomletMain.DocumentFrom(data);
+        
+        File.WriteAllText(path, TomletMain.TomlStringFrom(document));
+    }
+
+    public object Load(string path, Type type)
+    {
+        var source = File.ReadAllText(path);
+        var parser = new TomlParser();
+        var tomlDocument = parser.Parse(source);
+        
+        return TomletMain.To(type, tomlDocument);
+    }
+}
