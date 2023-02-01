1e84795b12e71c713be84befdab3eb01c351be8f
furesoft <filmee24@gmail.com>
2023-02-01 08:56:10 +0100

    feat(3rd party): Add Xml Settings Provider

---
+++ Src/Libraries/AuroraModularis.Settings.Xml/BootstrapBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using AuroraModularis.Core;
+
+namespace AuroraModularis.Settings.Xml;
+
+public static class BootstrapBuilderExtensions
+{
+    public static IBootstrapBuilder UseXml(this IBootstrapBuilder builder)
+    {
+        return builder.WithSettingsProvider<XmlSettingsProvider>();
+    }
+}
---
+++ Src/Libraries/AuroraModularis.Settings.Xml/XmlSettingsProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+using AuroraModularis.Core;
+
+namespace AuroraModularis.Settings.Xml;
+
+public class XmlSettingsProvider : IModuleSettingsProvider
+{
+    public void Save(object data, string path)
+    {
+        var serializer = new XmlSerializer(data.GetType());
+        using var fileStream = File.OpenWrite(path);
+        
+        serializer.Serialize(fileStream, data);
+    }
+
+    public object Load(string path, Type type)
+    {
+        var serializer = new XmlSerializer(type);
+        using var fileStream = File.OpenRead(path);
+        
+        return serializer.Deserialize(fileStream);
+    }
+}
