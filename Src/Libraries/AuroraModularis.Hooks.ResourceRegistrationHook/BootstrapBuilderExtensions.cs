7f23d394331c21453e864dc2b2a228f37e6bad00
furesoft <filmee24@gmail.com>
2023-02-01 09:15:43 +0100

    feat(3rd party): Add ResourceLoading Hook

---
+++ Src/Libraries/AuroraModularis.Hooks.ResourceRegistrationHook/BootstrapBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using AuroraModularis.Core;
+
+namespace AuroraModularis.Hooks.ResourceRegistrationHook;
+
+public static class BootstrapBuilderExtensions
+{
+    public static IBootstrapBuilder AddResourceHook(IBootstrapBuilder builder)
+    {
+        return builder.WithHook<ModuleLoadingHook>();
+    }
+}
