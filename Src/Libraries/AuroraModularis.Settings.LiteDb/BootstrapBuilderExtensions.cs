4fafa42a2be50a47fcc6744e0817ae2cc3a8744f
furesoft <filmee24@gmail.com>
2023-02-01 09:04:40 +0100

    feat(3rd party): Add LiteDb Settings Provider

---
+++ Src/Libraries/AuroraModularis.Settings.LiteDb/BootstrapBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using AuroraModularis.Core;
+
+namespace AuroraModularis.Settings.LiteDb;
+
+public static class BootstrapBuilderExtensions
+{
+    public static IBootstrapBuilder UseLiteDb(this IBootstrapBuilder builder)
+    {
+        var provider = new LiteDbSettingsProvider(builder);
+
+        builder.Configuration.SettingsProvider = provider;
+        
+        return builder;
+    }
+}
